bd25b0b7a9552d2c2c4315723550579cee7b316a
Adebimpe96 <sannidebimpe30@gmail.com>
2022-11-25 05:36:12 +0100

    footer updated

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,9 +1,9 @@
-// import {CodeBracketIcon} from '@heroicons/react/24/outline'
+
 function Footer() {
   return (
     <div className="flex bg-slate-100 h-10 justify-center items-center shadow-sm">
       <p className="blue-text">&copy; {new Date().getFullYear()} &nbsp;</p>
-      {/* <CodeBracketIcon className='w-6 mx-4'/> */}
+      
       <p className="blue-text"> Xfix ng.</p>
     </div>
   );
